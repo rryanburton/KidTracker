ea40ccf16d86bb2e5622548969eae165d0ce8500
Ryan Burton <rryanburton@gmail.com>
2016-02-09 00:14:04 -0500

    added initial photos

---
+++ KidTracker/MealTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  MealTableViewCell.swift
+//  KidTracker
+//
+//  Created by 3delrb on 2/8/16.
+//  Copyright © 2016 KTcompany. All rights reserved.
+//
+
+import UIKit
+
+class MealTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
