d224315efa12a8d4b46e896382e646453e4adac9
Ryan Burton <rryanburton@gmail.com>
2016-02-09 16:47:47 -0500

    finished but debugging

--- KidTracker/MealTableViewController.swift
+++ KidTracker/MealTableViewController.swift
@@ -18,10 +18,15 @@ class MealTableViewController: UITableViewController {
         
         // Use the edit button item provided by the table view controller.
         navigationItem.leftBarButtonItem = editButtonItem()
-
-        // Load the sample data.
-        loadSampleMeals()
         
+        // Load any saved meals, otherwise load sample data.
+        if let savedMeals = loadMeals() {
+            meals += savedMeals
+        } else {
+            // Load the sample data.
+            loadSampleMeals()
+        }
+
     }
     
     func loadSampleMeals() {
@@ -91,6 +96,7 @@ class MealTableViewController: UITableViewController {
             
             // Delete the row from the data source
             meals.removeAtIndex(indexPath.row)
+            saveMeals()
             tableView.deleteRowsAtIndexPaths([indexPath], withRowAnimation: .Fade)
         } else if editingStyle == .Insert {
             
@@ -151,6 +157,25 @@ class MealTableViewController: UITableViewController {
                 meals.append(meal)
                 tableView.insertRowsAtIndexPaths([newIndexPath], withRowAnimation: .Bottom)
             }
+            // Save the meals
+            saveMeals()
+            
         }
     }
+
+    // MARK: NSCoding
+    
+    func saveMeals() {
+        let isSuccessfulSave = NSKeyedArchiver.archiveRootObject(meals, toFile: Meal.ArchiveURL.path!)
+        if !isSuccessfulSave {
+            print("Failed to save meals...")
+            
+        }
+    }
+    func loadMeals() -> [Meal]? {
+        return NSKeyedUnarchiver.unarchiveObjectWithFile(Meal.ArchiveURL.path!) as? [Meal]
+        
+    }
+    
 }
+
